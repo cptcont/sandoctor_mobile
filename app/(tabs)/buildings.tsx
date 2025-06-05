9c83daab383e6915b2f74a6d46f4f7ba3ff105ef
andrew <andrewanderson@mail.ru>
2025-06-06 02:45:16 +0500

    Доработан для тестов с уведомлениями

--- app/(tabs)/buildings.tsx
+++ app/(tabs)/buildings.tsx
@@ -1,18 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Platform } from "react-native";
-import * as Notifications from "expo-notifications";
-import * as Device from "expo-device";
-import { useEffect, useRef, useState } from "react";
+import { StyleSheet, View } from "react-native";
 import NotifTest from "@/components/NotifTest";
-
-// Настройка обработчика уведомлений
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: true, // Показывать уведомление, когда приложение активно
-        shouldPlaySound: true, // Воспроизводить звук
-        shouldSetBadge: false, // Не показывать бейдж
-    }),
-});
 
 export default function BuildingsScreen() {
     return (
